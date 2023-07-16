57f1ce0e672e9e38b022533a0f92da50fee8c59a
kehinde <oyewalekehinde34@gmail.com>
2023-07-16 23:17:48 +0100

    update to book_api

---
+++ book_model.go
@@ -0,0 +1,9 @@
+package main
+
+type Book struct {
+	ID            string `json:"id,omitempty" bson:"_id,omitempty"`
+	Title         string `json:"title,omitempty" bson:"title,omitempty"`
+	Author        string `json:"author,omitempty" bson:"author,omitempty"`
+	NoOfChapters  int    `json:"no_of_chapters,omitempty" bson:"no_of_chapters,omitempty"`
+	PublishedDate string `json:"published_date,omitempty" bson:"published_date,omitempty"`
+}
