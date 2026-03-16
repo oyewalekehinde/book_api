cf16018cff63f04fe7879762f3be44252636a21f
Oluwaseun Oyewale <oyewalekehinde34@gmail.com>
2026-03-16 12:56:24 +0000

    update book id

--- book_model.go
+++ book_model.go
@@ -1,9 +1,11 @@
 package main
 
+import "go.mongodb.org/mongo-driver/v2/bson"
+
 type Book struct {
-	ID            string `json:"id,omitempty" bson:"_id,omitempty"`
-	Title         string `json:"title,omitempty" bson:"title,omitempty"`
-	Author        string `json:"author,omitempty" bson:"author,omitempty"`
-	NoOfChapters  int    `json:"no_of_chapters,omitempty" bson:"no_of_chapters,omitempty"`
-	PublishedDate string `json:"published_date,omitempty" bson:"published_date,omitempty"`
+	ID            bson.ObjectID `json:"id,omitempty" bson:"_id,omitempty"`
+	Title         string        `json:"title,omitempty" bson:"title,omitempty"`
+	Author        string        `json:"author,omitempty" bson:"author,omitempty"`
+	NoOfChapters  int           `json:"no_of_chapters,omitempty" bson:"no_of_chapters,omitempty"`
+	PublishedDate string        `json:"published_date,omitempty" bson:"published_date,omitempty"`
 }
