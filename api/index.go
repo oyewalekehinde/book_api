09ee300971535627556afa51d1294b1fd58bb79d
Oluwaseun Oyewale <oyewalekehinde34@gmail.com>
2026-03-16 17:52:34 +0000

    redefine index

--- api/index.go
+++ api/index.go
@@ -3,6 +3,7 @@ package handler
 import "net/http"
 
 func Handler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
 	w.WriteHeader(http.StatusOK)
 	_, _ = w.Write([]byte("ok"))
 }
